b6349ec7cd178e463dac03288161f6ea9b5a4661
GitLab <gitlab@localhost>
2013-05-28 09:31:12 +0200

    fixed bad regex

--- lib/gitlab_shell.rb
+++ lib/gitlab_shell.rb
@@ -6,7 +6,9 @@ class GitlabShell
   attr_accessor :key_id, :repo_name, :git_cmd, :repos_path, :repo_name
 
   def initialize
-    @key_id = /key-[0-9]/.match(ARGV.join).to_s
+    puts "---"
+    puts ARGV
+    @key_id = /key-[0-9]+/.match(ARGV.join).to_s
     @origin_cmd = ENV['SSH_ORIGINAL_COMMAND']
     @repos_path = GitlabConfig.new.repos_path
   end
