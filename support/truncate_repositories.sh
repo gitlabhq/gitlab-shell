3b77882e02da5702754bd2a23bca17a6a7d83a07
Robert Schilling <rschilling@student.tugraz.at>
2013-09-13 10:55:16 +0200

    Add optional repo path, remove reference to gitolite repo, fixes #77

--- support/truncate_repositories.sh
+++ support/truncate_repositories.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-home_dir="/home/git"
+# $1 is an optional argument specifying the location of the repositories directory.
+# Defaults to /home/git/repositories if not provided
+
+
+home_dir="/home/git/repositories"
+src=${1:-"$home_dir"}
 
 echo "Danger!!! Data Loss"
 while true; do
-  read -p "Do you wish to delete all directories (except gitolite-admin.git) from $home_dir/repositories/ (y/n) ?:  " yn
+  read -p "Do you wish to delete all directories from $home_dir/ (y/n) ?:  " yn
   case $yn in
-    [Yy]* ) sh -c "find $home_dir/repositories/. -maxdepth 1  -not -name 'gitolite-admin.git' -not -name '.' | xargs rm -rf"; break;;
+    [Yy]* ) sh -c "find $home_dir/. -maxdepth 1 -not -name '.' | xargs rm -rf"; break;;
     [Nn]* ) exit;;
     * ) echo "Please answer yes or no.";;
   esac
