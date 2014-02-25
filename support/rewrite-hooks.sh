3bd2b660ff550e3eddea5b4b113ee79cf66e0f7f
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-02-25 15:51:15 +0100

    Make support/rewrite-hooks.sh use bin/create_hooks

--- support/rewrite-hooks.sh
+++ support/rewrite-hooks.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
+# This script is deprecated. Use bin/create-hooks instead.
 
-# $1 is an optional argument specifying the location of the repositories directory.
-# Defaults to /home/git/repositories if not provided
-
-home_dir="/home/git"
-src=${1:-"$home_dir/repositories"}
-
-function create_link_in {
-  ln -s -f "$home_dir/gitlab-shell/hooks/update" "$1/hooks/update"
-}
-
-for dir in `ls "$src/"`
-do
-  if [ -d "$src/$dir" ]; then
-    if [[ "$dir" =~ ^.*\.git$ ]]
-    then
-      create_link_in "$src/$dir"
-    else
-      for subdir in `ls "$src/$dir/"`
-      do
-        if [ -d "$src/$dir/$subdir" ] && [[ "$subdir" =~ ^.*\.git$ ]]; then
-          create_link_in "$src/$dir/$subdir"
-        fi
-      done
-    fi
-  fi
-done
+gitlab_shell_dir="$(cd $(dirname $0) && pwd)/.."
+exec ${gitlab_shell_dir}/bin/create-hooks
