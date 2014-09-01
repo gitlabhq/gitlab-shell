fc16836764e9712a8f8d5b8ff844ae7512e08a82
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-09-01 19:04:27 +0300

    Add tests for GitlabPostReceive
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/gitlab_post_receive_spec.rb
+++ spec/gitlab_post_receive_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
-require 'gitlab_access'
+require 'gitlab_post_receive'
 
-describe GitlabAccess do
+describe GitlabPostReceive do
   let(:repository_path) { "/home/git/repositories" }
   let(:repo_name)   { 'dzaporozhets/gitlab-ci' }
   let(:repo_path)  { File.join(repository_path, repo_name) + ".git" }
-  let(:gitlab_access) { GitlabAccess.new(repo_path, 'key-123', 'wow') }
+  let(:gitlab_post_receive) { GitlabPostReceive.new(repo_path, 'key-123', 'wow') }
 
   before do
     GitlabConfig.any_instance.stub(repos_path: repository_path)
   end
 
   describe :initialize do
-    it { gitlab_access.repo_name.should == repo_name }
-    it { gitlab_access.repo_path.should == repo_path }
-    it { gitlab_access.changes.should == ['wow'] }
+    it { gitlab_post_receive.repo_path.should == repo_path }
+    it { gitlab_post_receive.changes.should == ['wow'] }
   end
 end
