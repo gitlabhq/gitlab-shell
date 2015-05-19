e36709f1826bd81e21cba27b6b58d1df96766175
Krzysiek Szczuka <kszczuka@gmail.com>
2015-05-19 11:46:06 +0200

    linebreaks

--- lib/gitlab_access.rb
+++ lib/gitlab_access.rb
@@ -19,7 +19,12 @@ class GitlabAccess
 
   def exec
     begin
-      status = api.check_access('git-receive-pack', @repo_name, @actor, @changes.to_a)
+      status = api.check_access(
+                                'git-receive-pack', 
+                                @repo_name, 
+                                @actor, 
+                                @changes.to_a
+                                )
 
       return true if status.allowed?
 
