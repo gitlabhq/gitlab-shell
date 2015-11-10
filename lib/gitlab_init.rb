96f0845952391531cc8bfc6b5ae13a6a65772e05
Pirate Praveen <praveen@debian.org>
2015-11-10 12:52:23 -0500

    use SHELL_ROOT_PATH variable to set ROOT_PATH

--- lib/gitlab_init.rb
+++ lib/gitlab_init.rb
@@ -1,3 +1,7 @@
-ROOT_PATH = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+if ENV['SHELL_ROOT_PATH'].nil? || ENV['SHELL_ROOT_PATH'].empty?
+  ROOT_PATH = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+else
+  ROOT_PATH = ENV['SHELL_ROOT_PATH']
+end
 
 require_relative 'gitlab_config'
