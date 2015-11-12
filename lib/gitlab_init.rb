f532377f9a183654bb2cd6c37134eaf24f64cbca
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-11-12 15:05:10 +0100

    Revert "Merge branch 'use-load-path' into 'master'"
    This reverts commit ae498b6cd4122d3d7f35e6b73b50c53615ca3488, reversing
    changes made to 79fdf65c71e90773fbf52d6832b74cf5a7124755.

--- lib/gitlab_init.rb
+++ lib/gitlab_init.rb
@@ -1,7 +1,3 @@
-if ENV['SHELL_ROOT_PATH'].nil? || ENV['SHELL_ROOT_PATH'].empty?
-  ROOT_PATH = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-else
-  ROOT_PATH = ENV['SHELL_ROOT_PATH']
-end
+ROOT_PATH = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
 require_relative 'gitlab_config'
