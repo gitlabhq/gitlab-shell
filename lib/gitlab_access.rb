f85d650e67d199f43259a9181d7c0406571f4bcf
Rémy Coutable <remy@rymai.me>
2017-02-06 09:51:05 +0100

    Bump VERSION to 3.6.7
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/gitlab_access.rb
+++ lib/gitlab_access.rb
@@ -26,7 +26,7 @@ class GitlabAccess
       "GIT_OBJECT_DIRECTORY" => ENV["GIT_OBJECT_DIRECTORY"]
     }
 
-    status = api.check_access('git-receive-pack', @repo_path, @actor, @changes, @protocol, env: env.to_json)
+    status = api.check_access('git-receive-pack', @repo_name, @actor, @changes, @protocol, env: env.to_json)
 
     raise AccessDeniedError, status.message unless status.allowed?
 
