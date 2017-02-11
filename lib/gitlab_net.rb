8bbda09f287d83b495dd58796e4421cdd9d498e3
Amin Mirzaee <amin@aym.co>
2017-02-11 14:21:32 -0500

    cleaner

--- lib/gitlab_net.rb
+++ lib/gitlab_net.rb
@@ -151,18 +151,15 @@ class GitlabNet
 
   def http_request_for(method, uri, params = {})
     request_klass = method == :get ? Net::HTTP::Get : Net::HTTP::Post
-
-    params.merge!(secret_token: secret_token)
-
-    uri.query = URI.encode_www_form( params ) if method == :get
-
+    if method == :get      
+        uri.query = URI.encode_www_form(secret_token: secret_token) + "&" + (uri.query || "")
     request = request_klass.new(uri.request_uri)
 
     user = config.http_settings['user']
     password = config.http_settings['password']
     request.basic_auth(user, password) if user && password
 
-    request.set_form_data(params)
+    request.set_form_data(params.merge(secret_token: secret_token))
 
     request
   end
