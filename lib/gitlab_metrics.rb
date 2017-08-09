63eba2167cfc5306cade31702300949cb76497e9
Gabriel Mazetto <brodock@gmail.com>
2017-08-09 08:54:02 +0200

    Support old versions of ruby without monotonic clock

--- lib/gitlab_metrics.rb
+++ lib/gitlab_metrics.rb
@@ -21,7 +21,11 @@ module GitlabMetrics
     #
     # Returns the time as a Fixnum.
     def self.monotonic_time
-      Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond)
+      if defined?(Process::CLOCK_MONOTONIC)
+        Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond)
+      else
+        Process.clock_gettime(Process::CLOCK_REALTIME, :millisecond)
+      end
     end
   end
 
