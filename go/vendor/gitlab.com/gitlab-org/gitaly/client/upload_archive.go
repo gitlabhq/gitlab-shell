50d1aadb120bf58dd910c1cd5c2f8b04595e155a
Andrew Newdigate <andrew@gitlab.com>
2018-12-07 15:33:52 +0200

    Gitaly client update

--- go/vendor/gitlab.com/gitlab-org/gitaly/client/upload_archive.go
+++ go/vendor/gitlab.com/gitlab-org/gitaly/client/upload_archive.go
@@ -3,20 +3,18 @@ package client
 import (
 	"io"
 
+	"gitlab.com/gitlab-org/gitaly-proto/go/gitalypb"
 	"gitlab.com/gitlab-org/gitaly/streamio"
-
-	pb "gitlab.com/gitlab-org/gitaly-proto/go/gitalypb"
-
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
 // UploadArchive proxies an SSH git-upload-archive (git archive --remote) session to Gitaly
-func UploadArchive(ctx context.Context, conn *grpc.ClientConn, stdin io.Reader, stdout, stderr io.Writer, req *pb.SSHUploadArchiveRequest) (int32, error) {
+func UploadArchive(ctx context.Context, conn *grpc.ClientConn, stdin io.Reader, stdout, stderr io.Writer, req *gitalypb.SSHUploadArchiveRequest) (int32, error) {
 	ctx2, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	ssh := pb.NewSSHServiceClient(conn)
+	ssh := gitalypb.NewSSHServiceClient(conn)
 	stream, err := ssh.SSHUploadArchive(ctx2)
 	if err != nil {
 		return 0, err
@@ -27,7 +25,7 @@ func UploadArchive(ctx context.Context, conn *grpc.ClientConn, stdin io.Reader, 
 	}
 
 	inWriter := streamio.NewWriter(func(p []byte) error {
-		return stream.Send(&pb.SSHUploadArchiveRequest{Stdin: p})
+		return stream.Send(&gitalypb.SSHUploadArchiveRequest{Stdin: p})
 	})
 
 	return streamHandler(func() (stdoutStderrResponse, error) {
--- go/vendor/gitlab.com/gitlab-org/gitaly/client/upload_pack.go
+++ go/vendor/gitlab.com/gitlab-org/gitaly/client/upload_pack.go
@@ -3,20 +3,18 @@ package client
 import (
 	"io"
 
+	"gitlab.com/gitlab-org/gitaly-proto/go/gitalypb"
 	"gitlab.com/gitlab-org/gitaly/streamio"
-
-	pb "gitlab.com/gitlab-org/gitaly-proto/go/gitalypb"
-
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
 // UploadPack proxies an SSH git-upload-pack (git fetch) session to Gitaly
-func UploadPack(ctx context.Context, conn *grpc.ClientConn, stdin io.Reader, stdout, stderr io.Writer, req *pb.SSHUploadPackRequest) (int32, error) {
+func UploadPack(ctx context.Context, conn *grpc.ClientConn, stdin io.Reader, stdout, stderr io.Writer, req *gitalypb.SSHUploadPackRequest) (int32, error) {
 	ctx2, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	ssh := pb.NewSSHServiceClient(conn)
+	ssh := gitalypb.NewSSHServiceClient(conn)
 	stream, err := ssh.SSHUploadPack(ctx2)
 	if err != nil {
 		return 0, err
@@ -27,7 +25,7 @@ func UploadPack(ctx context.Context, conn *grpc.ClientConn, stdin io.Reader, std
 	}
 
 	inWriter := streamio.NewWriter(func(p []byte) error {
-		return stream.Send(&pb.SSHUploadPackRequest{Stdin: p})
+		return stream.Send(&gitalypb.SSHUploadPackRequest{Stdin: p})
 	})
 
 	return streamHandler(func() (stdoutStderrResponse, error) {
