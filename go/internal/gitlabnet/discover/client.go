a36cf8de6c2b5c03c68a54e207190627b5c651f2
Igor Drozdov <idrozdov@gitlab.com>
2019-05-20 12:35:03 +0300

    Introduce gitlabnet.ParseJSON to DRY

--- go/internal/gitlabnet/discover/client.go
+++ go/internal/gitlabnet/discover/client.go
@@ -1,7 +1,6 @@
 package discover
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -32,56 +31,39 @@ func NewClient(config *config.Config) (*Client, error) {
 }
 
 func (c *Client) GetByCommandArgs(args *commandargs.CommandArgs) (*Response, error) {
-	if args.GitlabKeyId != "" {
-		return c.GetByKeyId(args.GitlabKeyId)
-	} else if args.GitlabUsername != "" {
-		return c.GetByUsername(args.GitlabUsername)
+	params := url.Values{}
+	if args.GitlabUsername != "" {
+		params.Add("username", args.GitlabUsername)
+	} else if args.GitlabKeyId != "" {
+		params.Add("key_id", args.GitlabKeyId)
 	} else {
 		// There was no 'who' information, this  matches the ruby error
 		// message.
 		return nil, fmt.Errorf("who='' is invalid")
 	}
-}
-
-func (c *Client) GetByKeyId(keyId string) (*Response, error) {
-	params := url.Values{}
-	params.Add("key_id", keyId)
 
 	return c.getResponse(params)
-}
-
-func (c *Client) GetByUsername(username string) (*Response, error) {
-	params := url.Values{}
-	params.Add("username", username)
-
-	return c.getResponse(params)
-}
-
-func (c *Client) parseResponse(resp *http.Response) (*Response, error) {
-	parsedResponse := &Response{}
-
-	if err := json.NewDecoder(resp.Body).Decode(parsedResponse); err != nil {
-		return nil, err
-	} else {
-		return parsedResponse, nil
-	}
 }
 
 func (c *Client) getResponse(params url.Values) (*Response, error) {
 	path := "/discover?" + params.Encode()
-	response, err := c.client.Get(path)
 
+	response, err := c.client.Get(path)
 	if err != nil {
 		return nil, err
 	}
-
 	defer response.Body.Close()
-	parsedResponse, err := c.parseResponse(response)
-	if err != nil {
-		return nil, fmt.Errorf("Parsing failed")
+
+	return parse(response)
+}
+
+func parse(hr *http.Response) (*Response, error) {
+	response := &Response{}
+	if err := gitlabnet.ParseJSON(hr, response); err != nil {
+		return nil, err
 	}
 
-	return parsedResponse, nil
+	return response, nil
 }
 
 func (r *Response) IsAnonymous() bool {
