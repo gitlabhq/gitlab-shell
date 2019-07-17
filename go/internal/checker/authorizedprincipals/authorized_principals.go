0c3a85b48bae26976221e9ddb363ff81c1f19cbf
Patrick Bajao <ebajao@gitlab.com>
2019-07-17 19:14:42 +0800

    Migrate gitlab-shell-authorized-principals-check
    A symlink to gitlab-shell is added and the ruby script has been
    renamed to `gitlab-shell-authorized-principals-check-ruby`. It'll
    be used as a fallback executable.

--- go/internal/checker/authorizedprincipals/authorized_principals.go
+++ go/internal/checker/authorizedprincipals/authorized_principals.go
@@ -1,6 +1,10 @@
 package authorizedprincipals
 
 import (
+	"errors"
+	"fmt"
+	"path"
+
 	"gitlab.com/gitlab-org/gitlab-shell/go/internal/command/readwriter"
 	"gitlab.com/gitlab-org/gitlab-shell/go/internal/config"
 )
@@ -12,5 +16,49 @@ type Checker struct {
 }
 
 func (c *Checker) Execute() error {
+	if err := c.validateArguments(); err != nil {
+		return err
+	}
+
+	args := c.Args
+	keyId := args[0]
+	principals := args[1:]
+
+	c.printPrincipalLines(keyId, principals)
+
+	return nil
+}
+
+func (c *Checker) printPrincipalLines(keyId string, principals []string) {
+	command := fmt.Sprintf("%s username-%s", path.Join(c.Config.RootDir, "bin", "gitlab-shell"), keyId)
+
+	for _, principal := range principals {
+		principalLine := fmt.Sprintf(`command="%s",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty %s`, command, principal)
+
+		fmt.Fprintln(c.ReadWriter.Out, principalLine)
+	}
+}
+
+func (c *Checker) validateArguments() error {
+	args := c.Args
+	argsSize := len(args)
+
+	if argsSize < 2 {
+		return errors.New(fmt.Sprintf("# Wrong number of arguments. %d. Usage\n#\tgitlab-shell-authorized-principals-check <key-id> <principal1> [<principal2>...]", argsSize))
+	}
+
+	keyId := args[0]
+	principals := args[1:]
+
+	if keyId == "" {
+		return errors.New("# No key_id provided")
+	}
+
+	for _, principal := range principals {
+		if principal == "" {
+			return errors.New("# An invalid principal was provided")
+		}
+	}
+
 	return nil
 }
---
+++ go/internal/checker/authorizedprincipals/authorized_principals_test.go
@@ -0,0 +1,86 @@
+package authorizedprincipals
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"gitlab.com/gitlab-org/gitlab-shell/go/internal/command/readwriter"
+	"gitlab.com/gitlab-org/gitlab-shell/go/internal/config"
+)
+
+func TestExecute(t *testing.T) {
+	testCases := []struct {
+		desc           string
+		arguments      []string
+		expectedOutput string
+	}{
+		{
+			desc:           "With single principal",
+			arguments:      []string{"key", "principal"},
+			expectedOutput: "command=\"/tmp/bin/gitlab-shell username-key\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty principal\n",
+		},
+		{
+			desc:          "With mulitple principals",
+			arguments:      []string{"key", "principal-1", "principal-2"},
+			expectedOutput: "command=\"/tmp/bin/gitlab-shell username-key\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty principal-1\ncommand=\"/tmp/bin/gitlab-shell username-key\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty principal-2\n",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			buffer := &bytes.Buffer{}
+			cmd := &Checker{
+				Config:     &config.Config{RootDir: "/tmp"},
+				Args:       tc.arguments,
+				ReadWriter: &readwriter.ReadWriter{Out: buffer},
+			}
+
+			err := cmd.Execute()
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedOutput, buffer.String())
+		})
+	}
+}
+
+func TestFailingExecute(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		arguments     []string
+		expectedError string
+	}{
+		{
+			desc:          "With wrong number of arguments",
+			arguments:     []string{"key"},
+			expectedError: "# Wrong number of arguments. 1. Usage\n#\tgitlab-shell-authorized-principals-check <key-id> <principal1> [<principal2>...]",
+		},
+		{
+			desc:          "With missing key_id",
+			arguments:     []string{"", "principal"},
+			expectedError: "# No key_id provided",
+		},
+		{
+			desc:          "With blank principal",
+			arguments:     []string{"key", "principal", ""},
+			expectedError: "# An invalid principal was provided",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			buffer := &bytes.Buffer{}
+			cmd := &Checker{
+				Config:     &config.Config{},
+				Args:       tc.arguments,
+				ReadWriter: &readwriter.ReadWriter{Out: buffer},
+			}
+
+			err := cmd.Execute()
+
+			require.Empty(t, buffer.String())
+			require.EqualError(t, err, tc.expectedError)
+		})
+	}
+}
