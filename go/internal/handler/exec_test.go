67b25d66608c0ed7955c7851ad45c28ef9b85c1d
Igor <idrozdov@gitlab.com>
2019-10-18 10:44:01 +0000

    Remove handler functions which aren't used

--- go/internal/handler/exec_test.go
+++ go/internal/handler/exec_test.go
@@ -2,102 +2,41 @@ package handler
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"gitlab.com/gitlab-org/gitlab-shell/go/internal/testhelper"
 	"google.golang.org/grpc"
+
+	"gitlab.com/gitlab-org/gitlab-shell/go/internal/config"
 )
 
-func TestInteralRunHandler(t *testing.T) {
-	type testCase struct {
-		name    string
-		args    []string
-		handler func(context.Context, *grpc.ClientConn, string) (int32, error)
-		want    int
-		wantErr bool
+func makeHandler(t *testing.T, err error) func(context.Context, *grpc.ClientConn) (int32, error) {
+	return func(ctx context.Context, client *grpc.ClientConn) (int32, error) {
+		require.NotNil(t, ctx)
+		require.NotNil(t, client)
+
+		return 0, err
+	}
+}
+
+func TestRunGitalyCommand(t *testing.T) {
+	cmd := GitalyCommand{
+		Config:  &config.Config{},
+		Address: "tcp://localhost:9999",
 	}
 
-	var currentTest *testCase
-	makeHandler := func(r1 int32, r2 error) func(context.Context, *grpc.ClientConn, string) (int32, error) {
-		return func(ctx context.Context, client *grpc.ClientConn, requestJSON string) (int32, error) {
-			require.NotNil(t, ctx)
-			require.NotNil(t, client)
-			require.Equal(t, currentTest.args[2], requestJSON)
-			return r1, r2
-		}
-	}
-	tests := []testCase{
-		{
-			name:    "expected",
-			args:    []string{"test", "tcp://localhost:9999", "{}"},
-			handler: makeHandler(0, nil),
-			want:    0,
-			wantErr: false,
-		},
-		{
-			name:    "handler_error",
-			args:    []string{"test", "tcp://localhost:9999", "{}"},
-			handler: makeHandler(0, fmt.Errorf("error")),
-			want:    0,
-			wantErr: true,
-		},
-		{
-			name:    "handler_exitcode",
-			args:    []string{"test", "tcp://localhost:9999", "{}"},
-			handler: makeHandler(1, nil),
-			want:    1,
-			wantErr: false,
-		},
-		{
-			name:    "handler_error_exitcode",
-			args:    []string{"test", "tcp://localhost:9999", "{}"},
-			handler: makeHandler(1, fmt.Errorf("error")),
-			want:    1,
-			wantErr: true,
-		},
-		{
-			name:    "too_few_arguments",
-			args:    []string{"test"},
-			handler: makeHandler(10, nil),
-			want:    1,
-			wantErr: true,
-		},
-		{
-			name:    "too_many_arguments",
-			args:    []string{"test", "1", "2", "3"},
-			handler: makeHandler(10, nil),
-			want:    1,
-			wantErr: true,
-		},
-		{
-			name:    "empty_gitaly_address",
-			args:    []string{"test", "", "{}"},
-			handler: makeHandler(10, nil),
-			want:    1,
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			currentTest = &tt
-			defer func() {
-				currentTest = nil
-			}()
+	err := cmd.RunGitalyCommand(makeHandler(t, nil))
+	require.NoError(t, err)
 
-			done, err := testhelper.PrepareTestRootDir()
-			defer done()
-			require.NoError(t, err)
+	expectedErr := errors.New("error")
+	err = cmd.RunGitalyCommand(makeHandler(t, expectedErr))
+	require.Equal(t, err, expectedErr)
+}
 
-			got, err := internalRunGitalyCommand(tt.args, tt.handler)
-			if tt.wantErr {
-				require.Error(t, err)
-			} else {
-				require.NoError(t, err)
-			}
+func TestMissingGitalyAddress(t *testing.T) {
+	cmd := GitalyCommand{Config: &config.Config{}}
 
-			require.Equal(t, tt.want, got)
-		})
-	}
+	err := cmd.RunGitalyCommand(makeHandler(t, nil))
+	require.EqualError(t, err, "no gitaly_address given")
 }
