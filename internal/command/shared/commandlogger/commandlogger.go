12c6711f321dd4b0acf507041cf05fbaa623e29e
David Kim <dkim@gitlab.com>
2020-04-03 17:02:01 +1030

    Change git command logging keys to be be snake cased

--- internal/command/shared/commandlogger/commandlogger.go
+++ internal/command/shared/commandlogger/commandlogger.go
@@ -9,11 +9,11 @@ import (
 func Log(command string, repository *pb.Repository, response *accessverifier.Response, protocol string) {
 	fields := log.Fields{
 		"command":       command,
-		"glProjectPath": repository.GlProjectPath,
-		"glRepository":  repository.GlRepository,
-		"userId":        response.UserId,
-		"userName":      response.Username,
-		"gitProtocol":   protocol,
+		"gl_project_path": repository.GlProjectPath,
+		"gl_repository":  repository.GlRepository,
+		"user_id":        response.UserId,
+		"username":      response.Username,
+		"git_protocol":   protocol,
 	}
 
 	log.WithFields(fields).Info("executing git command")
