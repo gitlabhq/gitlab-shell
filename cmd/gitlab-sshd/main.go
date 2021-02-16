a300cf87a12644b49b260287f0b26d2800049b34
Ben Kochie <superq@gmail.com>
2021-02-16 10:35:11 +0100

    Add monitoring endpoint to sshd
    Add a basic monitoring endpoint to the sshd command.
    * Listen on localhost port 9122 by default.
    * Integrate build/version info.
    * Update example config.
    
    https://gitlab.com/gitlab-org/gitlab-shell/-/issues/121
    
    Signed-off-by: Ben Kochie <superq@gmail.com>

--- cmd/gitlab-sshd/main.go
+++ cmd/gitlab-sshd/main.go
@@ -9,10 +9,16 @@ import (
 	"gitlab.com/gitlab-org/gitlab-shell/internal/config"
 	"gitlab.com/gitlab-org/gitlab-shell/internal/logger"
 	"gitlab.com/gitlab-org/gitlab-shell/internal/sshd"
+	"gitlab.com/gitlab-org/labkit/monitoring"
 )
 
 var (
 	configDir = flag.String("config-dir", "", "The directory the config is in")
+
+	// BuildTime signifies the time the binary was build.
+	BuildTime = "2021-02-16T09:28:07+01:00" // Set at build time in the Makefile
+	// Version is the current version of GitLab Shell sshd.
+	Version = "(unknown version)" // Set at build time in the Makefile
 )
 
 func overrideConfigFromEnvironment(cfg *config.Config) {
@@ -49,6 +55,18 @@ func main() {
 		log.Fatalf("configuration error: %v", err)
 	}
 	logger.ConfigureStandalone(cfg)
+
+	// Startup monitoring endpoint.
+	if cfg.Server.WebListen != "" {
+		go func() {
+			log.Fatal(
+				monitoring.Start(
+					monitoring.WithListenerAddress(cfg.Server.WebListen),
+					monitoring.WithBuildInformation(Version, BuildTime),
+				),
+			)
+		}()
+	}
 
 	if err := sshd.Run(cfg); err != nil {
 		log.Fatalf("Failed to start GitLab built-in sshd: %v", err)
