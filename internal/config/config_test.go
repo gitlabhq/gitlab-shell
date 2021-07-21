368a64546c97abdc7019f98cbc97eecdbda2b6a5
Igor Drozdov <idrozdov@gitlab.com>
2021-07-21 11:24:00 +0300

    Prometheus metrics for HTTP requests
    A RoundTripper for tracking the duration of an http request
    is introduced

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -5,8 +5,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"gitlab.com/gitlab-org/gitlab-shell/internal/testhelper"
+	"gitlab.com/gitlab-org/gitlab-shell/client/testserver"
 )
 
 func TestConfigApplyGlobalState(t *testing.T) {
@@ -21,4 +23,28 @@ func TestConfigApplyGlobalState(t *testing.T) {
 	config.ApplyGlobalState()
 
 	require.Equal(t, "foo", os.Getenv("SSL_CERT_DIR"))
+}
+
+func TestHttpClient(t *testing.T) {
+	url := testserver.StartHttpServer(t, []testserver.TestRequestHandler{})
+
+	config := &Config{GitlabUrl: url}
+	client := config.HttpClient()
+
+	_, err := client.Get("http://host.com/path")
+	require.NoError(t, err)
+
+	ms, err := prometheus.DefaultGatherer.Gather()
+	require.NoError(t, err)
+
+	lastMetric := ms[0]
+	require.Equal(t, lastMetric.GetName(), "gitlab_shell_http_request_seconds")
+
+	labels := lastMetric.GetMetric()[0].Label
+
+	require.Equal(t, "code", labels[0].GetName())
+	require.Equal(t, "404", labels[0].GetValue())
+
+	require.Equal(t, "method", labels[1].GetName())
+	require.Equal(t, "get", labels[1].GetValue())
 }
--- internal/sshd/connection.go
+++ internal/sshd/connection.go
@@ -4,47 +4,11 @@ import (
 	"context"
 	"time"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/sync/semaphore"
-)
 
-const (
-	namespace     = "gitlab_shell"
-	sshdSubsystem = "sshd"
-)
-
-var (
-	sshdConnectionDuration = promauto.NewHistogram(
-		prometheus.HistogramOpts{
-			Namespace: namespace,
-			Subsystem: sshdSubsystem,
-			Name:      "connection_duration_seconds",
-			Help:      "A histogram of latencies for connections to gitlab-shell sshd.",
-			Buckets: []float64{
-				0.005, /* 5ms */
-				0.025, /* 25ms */
-				0.1,   /* 100ms */
-				0.5,   /* 500ms */
-				1.0,   /* 1s */
-				10.0,  /* 10s */
-				30.0,  /* 30s */
-				60.0,  /* 1m */
-				300.0, /* 5m */
-			},
-		},
-	)
-
-	sshdHitMaxSessions = promauto.NewCounter(
-		prometheus.CounterOpts{
-			Namespace: namespace,
-			Subsystem: sshdSubsystem,
-			Name:      "concurrent_limited_sessions_total",
-			Help:      "The number of times the concurrent sessions limit was hit in gitlab-shell sshd.",
-		},
-	)
+	"gitlab.com/gitlab-org/gitlab-shell/internal/metrics"
 )
 
 type connection struct {
@@ -64,7 +28,7 @@ func newConnection(maxSessions int64, remoteAddr string) *connection {
 }
 
 func (c *connection) handle(ctx context.Context, chans <-chan ssh.NewChannel, handler channelHandler) {
-	defer sshdConnectionDuration.Observe(time.Since(c.begin).Seconds())
+	defer metrics.SshdConnectionDuration.Observe(time.Since(c.begin).Seconds())
 
 	for newChannel := range chans {
 		if newChannel.ChannelType() != "session" {
@@ -73,7 +37,7 @@ func (c *connection) handle(ctx context.Context, chans <-chan ssh.NewChannel, ha
 		}
 		if !c.concurrentSessions.TryAcquire(1) {
 			newChannel.Reject(ssh.ResourceShortage, "too many concurrent sessions")
-			sshdHitMaxSessions.Inc()
+			metrics.SshdHitMaxSessions.Inc()
 			continue
 		}
 		channel, requests, err := newChannel.Accept()
