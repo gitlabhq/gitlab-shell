e6c46528958276558379fc2e8c4cccc6c9d5c6b5
kmcknight <kmcknight@gitlab.com>
2022-04-12 17:09:37 -0700

    Split out into manual/push

--- internal/command/twofactorverify/twofactorverifypush_test.go
+++ internal/command/twofactorverify/twofactorverifypush_test.go
@@ -17,10 +17,10 @@ import (
 	"gitlab.com/gitlab-org/gitlab-shell/internal/gitlabnet/twofactorverify"
 )
 
-func setup(t *testing.T) []testserver.TestRequestHandler {
+func setupPush(t *testing.T) []testserver.TestRequestHandler {
 	requests := []testserver.TestRequestHandler{
 		{
-			Path: "/api/v4/internal/two_factor_otp_check",
+			Path: "/api/v4/internal/two_factor_push_otp_check",
 			Handler: func(w http.ResponseWriter, r *http.Request) {
 				b, err := io.ReadAll(r.Body)
 				defer r.Body.Close()
@@ -54,12 +54,12 @@ func setup(t *testing.T) []testserver.TestRequestHandler {
 }
 
 const (
-	question    = "OTP: \n"
-	errorHeader = "OTP validation failed.\n"
+	pushQuestion    = "OTP: \n"
+	pushErrorHeader = "Push OTP validation failed.\n"
 )
 
-func TestExecute(t *testing.T) {
-	requests := setup(t)
+func TestExecutePush(t *testing.T) {
+	requests := setupPush(t)
 
 	url := testserver.StartSocketHttpServer(t, requests)
 
@@ -73,37 +73,31 @@ func TestExecute(t *testing.T) {
 			desc:           "When push is provided",
 			arguments:      &commandargs.Shell{GitlabKeyId: "1"},
 			answer:         "",
-			expectedOutput: question + "Push OTP validation successful. Git operations are now allowed.\n",
-		},
-		{
-			desc:           "With a known key id",
-			arguments:      &commandargs.Shell{GitlabKeyId: "1"},
-			answer:         "123456\n",
-			expectedOutput: question + "OTP validation successful. Git operations are now allowed.\n",
+			expectedOutput: pushQuestion + "Push OTP validation successful. Git operations are now allowed.\n",
 		},
 		{
 			desc:           "With bad response",
 			arguments:      &commandargs.Shell{GitlabKeyId: "-1"},
-			answer:         "123456\n",
-			expectedOutput: question + errorHeader + "Parsing failed\n",
+			answer:         "",
+			expectedOutput: pushQuestion + pushErrorHeader + "Parsing failed\n",
 		},
 		{
 			desc:           "With API returns an error",
 			arguments:      &commandargs.Shell{GitlabKeyId: "error"},
-			answer:         "yes\n",
-			expectedOutput: question + errorHeader + "error message\n",
+			answer:         "",
+			expectedOutput: pushQuestion + pushErrorHeader + "error message\n",
 		},
 		{
 			desc:           "With API fails",
 			arguments:      &commandargs.Shell{GitlabKeyId: "broken"},
-			answer:         "yes\n",
-			expectedOutput: question + errorHeader + "Internal API error (500)\n",
+			answer:         "",
+			expectedOutput: pushQuestion + pushErrorHeader + "Internal API error (500)\n",
 		},
 		{
 			desc:           "With missing arguments",
 			arguments:      &commandargs.Shell{},
-			answer:         "yes\n",
-			expectedOutput: question + errorHeader + "who='' is invalid\n",
+			answer:         "",
+			expectedOutput: pushQuestion + pushErrorHeader + "who='' is invalid",
 		},
 	}
 
@@ -111,10 +105,9 @@ func TestExecute(t *testing.T) {
 		t.Run(tc.desc, func(t *testing.T) {
 			output := &bytes.Buffer{}
 
-			input := bytes.NewBufferString(tc.answer)
-			if tc.answer == "" { // pushauth test must not provide input otherwise it gets treated as a manual OTP
-				input.Reset()
-			}
+			var input io.Reader
+			// make input wait for push auth tests
+			input, _ = io.Pipe()
 
 			cmd := &Command{
 				Config:     &config.Config{GitlabUrl: url},
