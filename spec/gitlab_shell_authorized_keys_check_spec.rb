af8327492a267ba74e9d6f4abe74ac88a51b94a6
Igor Drozdov <idrozdov@gitlab.com>
2023-07-05 07:43:18 +0000

    Draft: Test merge request

--- spec/gitlab_shell_authorized_keys_check_spec.rb
+++ spec/gitlab_shell_authorized_keys_check_spec.rb
@@ -4,8 +4,7 @@ describe 'bin/gitlab-shell-authorized-keys-check' do
   include_context 'gitlab shell'
 
   def tmp_socket_path
-    # This has to be a relative path shorter than 100 bytes due to
-    # limitations in how Unix sockets work.
+    # This has to be a relative path shorter than 100 bytes due to limitations in how Unix sockets work.
     'tmp/gitlab-shell-authorized-keys-check-socket'
   end
 
