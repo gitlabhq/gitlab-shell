f7fe1cceaf6a35ba3b6e3b3566ebb81df48aa50b
Ash McKenzie <amckenzie@gitlab.com>
2023-08-29 20:23:34 +1000

    Add new CountingWriter struct

--- internal/command/readwriter/readwriter.go
+++ internal/command/readwriter/readwriter.go
@@ -1,9 +1,24 @@
 package readwriter
 
-import "io"
+import (
+	"io"
+)
 
 type ReadWriter struct {
 	Out    io.Writer
 	In     io.Reader
 	ErrOut io.Writer
+}
+
+// CountingWriter wraps an io.Writer and counts all the writes. Accessing
+// the count N is not thread-safe.
+type CountingWriter struct {
+	W io.Writer
+	N int64
+}
+
+func (cw *CountingWriter) Write(p []byte) (int, error) {
+	n, err := cw.W.Write(p)
+	cw.N += int64(n)
+	return n, err
 }
