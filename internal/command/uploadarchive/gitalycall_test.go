66167f31ae3e5707820e248cf27d5dd2d0eda4d7
Vasilii Iakliushin <viakliushin@gitlab.com>
2026-09-04 11:36:58 +0200

    Deduplicate Gitaly integration test setup

--- internal/command/uploadarchive/gitalycall_test.go
+++ internal/command/uploadarchive/gitalycall_test.go
@@ -1,82 +1,35 @@
 package uploadarchive
 
 import (
-	"bytes"
-	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/labkit/correlation"
 
-	"gitlab.com/gitlab-org/gitlab-shell/v14/client/testserver"
 	"gitlab.com/gitlab-org/gitlab-shell/v14/internal/command/commandargs"
-	"gitlab.com/gitlab-org/gitlab-shell/v14/internal/command/readwriter"
-	"gitlab.com/gitlab-org/gitlab-shell/v14/internal/config"
-	"gitlab.com/gitlab-org/gitlab-shell/v14/internal/sshenv"
-	"gitlab.com/gitlab-org/gitlab-shell/v14/internal/testhelper/requesthandlers"
+	"gitlab.com/gitlab-org/gitlab-shell/v14/internal/testhelper/gitalytest"
 )
 
 func TestUploadArchive(t *testing.T) {
-	for _, network := range []string{"unix", "tcp", "dns"} {
-		t.Run(fmt.Sprintf("via %s network", network), func(t *testing.T) {
-			gitalyAddress, testServer := testserver.StartGitalyServer(t, network)
-			t.Logf("Server address: %s", gitalyAddress)
-
-			requests := requesthandlers.BuildAllowedWithGitalyHandlers(t, gitalyAddress)
-			url := testserver.StartHTTPServer(t, requests)
-
-			output := &bytes.Buffer{}
-			input := &bytes.Buffer{}
-
-			userID := "1"
-			repo := "group/repo"
-
-			env := sshenv.Env{
-				IsSSHConnection: true,
-				OriginalCommand: "git-upload-archive " + repo,
-				RemoteAddr:      "127.0.0.1",
-			}
-
-			args := &commandargs.Shell{
-				GitlabKeyID: userID,
-				CommandType: commandargs.UploadArchive,
-				SSHArgs:     []string{"git-upload-archive", repo},
-				Env:         env,
-			}
-
-			cfg := &config.Config{GitlabURL: url}
-			cfg.GitalyClient.InitSidechannelRegistry(context.Background())
-			cmd := &Command{
-				Config:     cfg,
-				Args:       args,
-				ReadWriter: &readwriter.ReadWriter{ErrOut: output, Out: output, In: input},
-			}
-
-			correlationID := correlation.SafeRandomID()
-			ctx := correlation.ContextWithCorrelation(context.Background(), correlationID)
-			ctx = correlation.ContextWithClientName(ctx, "gitlab-shell-tests")
-
-			_, err := cmd.Execute(ctx)
-			require.NoError(t, err)
-
-			require.Equal(t, "UploadArchive: "+repo, output.String())
-
-			for k, v := range map[string]string{
-				"gitaly-feature-cache_invalidator":        "true",
-				"gitaly-feature-inforef_uploadpack_cache": "false",
-				"x-gitlab-client-name":                    "gitlab-shell-tests-git-upload-archive",
-				"key_id":                                  "123",
-				"user_id":                                 "user-1",
-				"remote_ip":                               "127.0.0.1",
-				"key_type":                                "key",
-			} {
-				actual := testServer.ReceivedMD[k]
-				require.Len(t, actual, 1)
-				require.Equal(t, v, actual[0])
-			}
-			require.Empty(t, testServer.ReceivedMD["some-other-feature-flag"])
-			require.Equal(t, testServer.ReceivedMD["x-gitlab-correlation-id"][0], correlationID)
+	gitalytest.RunAllNetworks(t, func(t *testing.T, servers gitalytest.Servers) {
+		correlationID := correlation.SafeRandomID()
+		setup := gitalytest.NewCommandSetup(gitalytest.CommandOptions{
+			GitlabURL:     servers.GitlabURL,
+			CommandType:   commandargs.UploadArchive,
+			Repository:    "group/repo",
+			RemoteAddr:    "127.0.0.1",
+			GitlabKeyID:   "1",
+			CorrelationID: correlationID,
+			ClientName:    "gitlab-shell-tests",
 		})
-	}
+		cmd := &Command{Config: setup.Config, Args: setup.Args, ReadWriter: setup.ReadWriter}
+		_, err := cmd.Execute(setup.Context)
+		require.NoError(t, err)
+		require.Equal(t, "UploadArchive: group/repo", setup.Output.String())
+		gitalytest.RequireMetadata(t, servers.Gitaly, gitalytest.MetadataExpectations{
+			ClientNameSuffix: "git-upload-archive",
+			CorrelationID:    correlationID,
+			RemoteAddr:       "127.0.0.1",
+		})
+	})
 }
